0aacd5d073c3e57108641553c4519fbca4d38149
swe2mohamed <swe.mohamed1990@gmail.com>
2024-03-06 11:25:00 +0100

    Initial commit

---
+++ src/main/java/se/String_Exercises/App.java
@@ -0,0 +1,55 @@
+package se.String_Exercises;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /*
+        Introduction
+         */
+        LocalDate ld = LocalDate.of(2024, Month.FEBRUARY,6);
+        System.out.println(ld);
+
+        ld = ld.plusDays(1);
+        System.out.println(ld);
+
+        LocalTime lt = LocalTime.of(14,38);
+        System.out.println(lt);
+
+        LocalDateTime ldt = LocalDateTime.of(ld,lt);
+        System.out.println(ldt);
+
+        ZonedDateTime zdt = ZonedDateTime.of(ldt,ZoneId.of("Europe/Athens"));
+        System.out.println(zdt);
+
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        System.out.println(ld.format(dtf));
+        System.out.println(dtf.format(ld));
+
+
+        /*
+        Exercise 1:
+        Create a LocalDate of the current day and print it out
+         */
+        System.out.println("___________________");
+        LocalDate currentDate = LocalDate.now();
+        System.out.print("Q.1) ");
+        System.out.println("Today " + currentDate);
+
+
+
+
+        }
+
+
+
+    }
+
